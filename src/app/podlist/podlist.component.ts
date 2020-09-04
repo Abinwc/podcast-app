64d56f4f8820f32b5c4a4c6214157598b1f9bb21
abinwc <abinpaul5598@gmail.com>
2020-09-04 10:45:48 +0530

    second commit

--- src/app/podlist/podlist.component.ts
+++ src/app/podlist/podlist.component.ts
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./podlist.component.css']
 })
 export class PodlistComponent implements OnInit {
+    public s_no : number;
+    public First : string;
+    public Last : string;
+    public Handle : string; 
 
-  constructor() { }
+   
+
+  constructor() { 
+    this.s_no = 1;
+    this.First='Abin';
+    this.Last='wc';
+    this.Handle='@sae';
+  }
 
   ngOnInit(): void {
   }
